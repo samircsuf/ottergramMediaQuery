9d848fa916e458907c1c0c4a8475fb15205ab331
samircsuf <mandalsamir18@gmail.com>
2017-02-22 20:23:54 -0800

    Added Assignment# 3

---
+++ scripts/hijack.js
@@ -0,0 +1,8 @@
+var links = document.getElementsByTagName("a");
+for (var i = 0; i < links.length; i++) {
+    var thisLink = links[i];
+
+    thisLink.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+}
