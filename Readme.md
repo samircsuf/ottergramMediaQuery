2c4fc8de7dd14260623fff2f022b09a6e88c0d4e
samircsuf <mandalsamir18@gmail.com>
2017-03-01 21:24:33 -0800

    Revert "Adding Assignment#4"
    This reverts commit c8cb8b7ae2e61c1d32dda5e435fbb181c00f8978.

--- Readme.md
+++ Readme.md
@@ -1,17 +1,12 @@
-Assignment 4 - Spring 2017
+CPSC 473 - Web Front-End Engineering for Internet Applications
 
-Section 2 due February 27.  Section 1 due March 1.
+Assignment 2 - Spring 2017
 
-In this assignment, you will finish the Ottergram app and write some new JavaScript to add additional features.
+Section 2 due February 6.  Section 1 due February 8.
+
+In this assignment, you will work through Chapters 3 and 4 of the textbook, upload the result to GitHub, and play some games designed to teach you CSS.
 
 Complete the following:
 
-1.Work through Chapter 7.  Verify that you can toggle between showing and hiding the detail image, and that both visual effects are in place.
-
-2.In the current version of Ottergram, the detail image is changed when a thumbnail is clicked.  Users who prefer the keyboard can select thumbnails and trigger clicks using the Tab and Enter keys, but the interaction is awkward.
-
-Use JavaScript to add the ability to select a thumbnail and change the detail image by pressing a digit key (e.g., '1' to select Barry, and '5' to select Barbara in the default layout).
-
-Do not hard-code key-to-thumbnail mappings -- if the order or number of images changes, your code should continue to function correctly (at least for the first 9 thumbnails).
-
-3.Push the contents of your ottergram directory into a new public GitHub repository.
+Work through Chapters 3 and 4, copying in files from front-end-dev-resources and updating files as necessary.
+Confirm that your result matches the Ottergram page shown in the book.
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-var HIDDEN_DETAIL_CLASS = 'hidden-detail';
-var TINY_EFFECT_CLASS = 'is-tiny';
-var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -34,7 +30,6 @@ function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
-        showDetails();
     });
 }
 
@@ -45,58 +40,9 @@ function getThumbnailsArray() {
     return thumbnailArray;
 }
 
-function hideDetails() {
-    'use strict';
-    document.body.classList.add(HIDDEN_DETAIL_CLASS);
-}
-
-function showDetails() {
-    'use strict';
-    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
-    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
-    frame.classList.add(TINY_EFFECT_CLASS);
-    setTimeout(function() {
-        frame.classList.remove(TINY_EFFECT_CLASS);
-    }, 50);
-}
-
-function addKeyPressHandler() {
-    'use strict';
-    document.body.addEventListener('keyup', function(event) {
-        event.preventDefault();
-        console.log(event.keyCode);
-        if (event.keyCode === ESC_KEY) {
-            hideDetails();
-        }
-    });
-}
-
-function addNumericKeypad(thumb, index) {
-    'use strict';
-    //var a = index;
-    document.body.addEventListener('keyup', function(event) {
-        event.preventDefault();
-        if (event.keyCode >= 49 && event.keyCode <= 57) {
-            if (event.keyCode == 49 + index) {
-                setDetailsFromThumb(thumb);
-                showDetails();
-            }
-        }
-        if (event.keyCode >= 97 && event.keyCode <= 105) {
-            if (event.keyCode == 97 + index) {
-                setDetailsFromThumb(thumb);
-                showDetails();
-            }
-        }
-    });
-}
-
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
-    thumbnails.forEach(addNumericKeypad); //'.foreach' returns the current element and index
-
     thumbnails.forEach(addThumbClickHandler);
-    addKeyPressHandler();
 }
 initializeEvents();
